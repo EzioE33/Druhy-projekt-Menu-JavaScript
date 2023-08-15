991e8b3bd8a33269fb969f4d3dafcf4f47b99b4a
EzioE33 <134432527+EzioE33@users.noreply.github.com>
2023-08-15 09:28:17 +0200

    Můj druhý projekt - hamburger menu s animací

---
+++ Project druhy - responzivní menu v JavaScriptu/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger")
+const navBar = document.querySelector(".nav-bar")
+
+
+hamburger.onclick = function () {
+    navBar.classList.toggle("active")
+    hamburger.classList.toggle("change")
+}
